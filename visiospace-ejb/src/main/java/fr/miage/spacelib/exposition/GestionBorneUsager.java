87bf70ee110dd9e960ecd7926e2e16a3c43a362b
vivalx <alexis.vivier@toulouse.miage.fr>
2021-06-13 13:32:57 +0200

    Prise en charge de ReservationExport

--- visiospace-ejb/src/main/java/fr/miage/spacelib/exposition/GestionBorneUsager.java
+++ visiospace-ejb/src/main/java/fr/miage/spacelib/exposition/GestionBorneUsager.java
@@ -5,8 +5,15 @@
  */
 package fr.miage.spacelib.exposition;
 
+import fr.miage.spacelib.entities.Quai;
+import fr.miage.spacelib.entities.Reservation;
+import fr.miage.spacelib.entities.Usager;
 import fr.miage.spacelib.metier.GestionReservationLocal;
 import fr.miage.spacelib.vspaceshared.interfremote.GestionBorneUsagerRemote;
+import fr.miage.spacelib.vspaceshared.utilities.QuaiExport;
+import fr.miage.spacelib.vspaceshared.utilities.ReservationExport;
+import fr.miage.spacelib.vspaceshared.utilities.StationExport;
+import fr.miage.spacelib.vspaceshared.utilities.UsagerExport;
 import java.util.Date;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -22,8 +29,17 @@ public class GestionBorneUsager implements GestionBorneUsagerRemote {
     private GestionReservationLocal gestionReservation;
 
     @Override
-    public void reserverVoyage(long idUsager, int nbPassagers, Date dateDepart, Date dateArrivee, long stationDepart, long stationArrivee) {
-        this.gestionReservation.reserverVoyage(idUsager, nbPassagers, dateDepart, dateArrivee, stationDepart, stationArrivee);
+    public ReservationExport reserverVoyage(long idUsager, int nbPassagers, Date dateDepart, Date dateArrivee, long stationDepart, long stationArrivee) {
+        Reservation res = this.gestionReservation.reserverVoyage(idUsager, nbPassagers, dateDepart, dateArrivee, stationDepart, stationArrivee);
+        Quai quaiDepart = res.getArrivee();
+        Quai quaiArrivee = res.getArrivee();
+        Usager usager = res.getUsager();
+        
+        return new ReservationExport(res.getId(), 
+                new QuaiExport(quaiDepart.getId(), new StationExport(quaiDepart.getStation().getId(), quaiDepart.getStation().getCoordonnee())),
+                new QuaiExport(quaiArrivee.getId(), new StationExport(quaiArrivee.getStation().getId(), quaiArrivee.getStation().getCoordonnee())), 
+                new UsagerExport(usager.getId(), usager.getNom(), usager.getPrenom()), 
+                res.getNbPassagers());
     }
 
     @Override
