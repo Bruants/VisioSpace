8fd40169780d9efff6d6b32b1c198f9ce7876e29
vivalx <alexis.vivier@toulouse.miage.fr>
2021-06-17 21:59:36 +0200

    ajout gestion nombre passager a la reservation de la navette

--- visiospace-ejb/src/main/java/fr/miage/spacelib/metier/GestionReservation.java
+++ visiospace-ejb/src/main/java/fr/miage/spacelib/metier/GestionReservation.java
@@ -28,6 +28,7 @@ import javax.ejb.Stateless;
 
 /**
  * Gestion des reservations des usagers
+ *
  * @author AlexisVivier
  */
 @Stateless
@@ -50,51 +51,49 @@ public class GestionReservation implements GestionReservationLocal {
 
     @EJB
     private StationFacadeLocal stationFacade;
-       
+
     /**
-     * Reserve le voyage d'un usager entre deux stations : 
-     *  - Trouver et reserver une navette disponible
-     *    à la station de départ
-     *  - Trouver et reserver un quai de libre 
-     *    à la station d'arrivée
-     * @param idUsager       Identifiant de l'usager à l'origine de la commande
-     * @param nbPassagers    Nombre de passager dans la navette
-     * @param dateDepart     Date de départ du voyage
-     * @param dateArrivee    Date d'arrivée du voyage
-     * @param stationDepart  Station de départ du voyage
+     * Reserve le voyage d'un usager entre deux stations : - Trouver et reserver
+     * une navette disponible à la station de départ - Trouver et reserver un
+     * quai de libre à la station d'arrivée
+     *
+     * @param idUsager Identifiant de l'usager à l'origine de la commande
+     * @param nbPassagers Nombre de passager dans la navette
+     * @param dateDepart Date de départ du voyage
+     * @param dateArrivee Date d'arrivée du voyage
+     * @param stationDepart Station de départ du voyage
      * @param stationArrivee Station d'arrivée du voyage
-     * @return reservation   Le voyage réservé 
+     * @return reservation Le voyage réservé
      */
     @Override
-    public Reservation reserverVoyage(long idUsager, int nbPassagers, 
-            Date dateDepart, Date dateArrivee, 
+    public Reservation reserverVoyage(long idUsager, int nbPassagers,
+            Date dateDepart, Date dateArrivee,
             long stationDepart, long stationArrivee)
-            
-            throws AucunQuaiException, AucuneStationException, 
-            AucuneNavetteException, NombrePlacesInvalideException, 
+            throws AucunQuaiException, AucuneStationException,
+            AucuneNavetteException, NombrePlacesInvalideException,
             AucunUsagerException, DateInvalideException, NombrePassagersInvalideException {
-        
-        if (usagerFacade.find(idUsager) == null) {
-            throw new AucunUsagerException();
-        }
-        
-        if (dateDepart.after(dateArrivee)) {
-            throw new DateInvalideException();
-        }
-                 
-        if (nbPassagers <= 0) {
-            throw new NombrePassagersInvalideException();
-        }
-        
-        if (stationFacade.find(stationDepart) == null 
-                || stationFacade.find(stationArrivee) == null) {
-            throw new AucuneStationException();
-        }
-                
-        
+
         Reservation reservation = new Reservation();
         Operation voyage = new Operation();
         Navette navette;
+        int nbPlacesNavette;
+
+        if (usagerFacade.find(idUsager) == null) {
+            throw new AucunUsagerException();
+        }
+
+        if (dateDepart.after(dateArrivee)) {
+            throw new DateInvalideException();
+        }
+
+        if (nbPassagers <= 0 || nbPassagers > 15) {
+            throw new NombrePassagersInvalideException();
+        }
+
+        if (stationFacade.find(stationDepart) == null
+                || stationFacade.find(stationArrivee) == null) {
+            throw new AucuneStationException();
+        }
 
         /* Création de l'opération voyage */
         voyage.setTypeOperation(Operation.TYPES.VOYAGE);
@@ -103,78 +102,83 @@ public class GestionReservation implements GestionReservationLocal {
         voyage.setDateDepart(dateDepart);
         voyage.setDateArrivee(dateArrivee);
         reservation.setVoyage(voyage);
-        
+
         /* Création de la réservation */
         reservation.setUsager(usagerFacade.find(idUsager));
-        
+
         reservation.setNbPassagers(nbPassagers);
-        
+
+        //Identifie le nombres de places a reszerver
+        nbPlacesNavette = nbPassagers <= 10 ? 10 : 15;
+        nbPlacesNavette = nbPassagers <= 5 ? 5 : -1;
+        nbPlacesNavette = nbPassagers <= 2 ? 2 : -1;
+
         //Recherche d'une navette correspondante
-        navette = gestionStation.navettesDispo(stationDepart, nbPassagers);
+        navette = gestionStation.navettesDispo(stationDepart, nbPlacesNavette);
         reservation.setUtilisee(navette);
-        
+
         reservation.setDepart(gestionNavette.quai(navette.getId()));
         //Recherche d'un quai de libre
         reservation.setArrivee(
                 gestionStation.reserverQuai(stationArrivee, navette.getId())
         );
-        
+
         reservationFacade.create(reservation);
-        
+
         return reservation;
     }
 
     /**
-     * Débute le voyage reservé
-     * La navette libére alors le quai 
-     * de la station de départ et une opération de départ
-     * est créer dans l'historique de la navette
+     * Débute le voyage reservé La navette libére alors le quai de la station de
+     * départ et une opération de départ est créer dans l'historique de la
+     * navette
+     *
      * @param idVoyage Identifiant du voyage courant
      */
     @Override
-    public void departVoyage(long idReservation) 
-            throws AucuneNavetteException, AucunVoyageException, 
+    public void departVoyage(long idReservation)
+            throws AucuneNavetteException, AucunVoyageException,
             AucunQuaiException {
         Navette navette = reservationFacade.find(idReservation).getUtilisee();
-        
+
         if (navette == null) {
             throw new AucuneNavetteException();
         }
-        
+
         gestionNavette.lancerNavette(navette.getId());
     }
 
     /**
-     * Termine le voyage initié
-     * La navette s'amarre alors au quai 
-     * de la station d'arrivée et une opération d'arrivée
-     * est créer dans l'historique de la navette
+     * Termine le voyage initié La navette s'amarre alors au quai de la station
+     * d'arrivée et une opération d'arrivée est créer dans l'historique de la
+     * navette
+     *
      * @param idVoyage Identifiant du voyage courant
      */
     @Override
-    public void arriveeVoyage(long idReservation) 
-     throws AucunVoyageException {
-        
+    public void arriveeVoyage(long idReservation)
+            throws AucunVoyageException {
+
         Reservation reservation = reservationFacade.find(idReservation);
-        
+
         if (reservation == null) {
             throw new AucunVoyageException();
         }
-        
+
         Operation voyage = reservation.getVoyage();
-        
+
         voyage.setTerminee(true);
         reservationFacade.edit(reservation);
     }
-    
+
     @Override
-    public List<Station> toutesStations(){
+    public List<Station> toutesStations() {
         return gestionStation.toutesStations();
     }
 
     @Override
     public Reservation lastReservation(long idUsager) throws AucunVoyageException {
-        if(reservationFacade.findUsager(idUsager).size() <= 0){
+        if (reservationFacade.findUsager(idUsager).size() <= 0) {
             throw new AucunVoyageException(" de l'utilisateur : " + idUsager);
         }
         return reservationFacade.findUsager(idUsager).get(0);
@@ -184,6 +188,5 @@ public class GestionReservation implements GestionReservationLocal {
     public Reservation trouver(long idReservation) {
         return reservationFacade.find(idReservation);
     }
-   
 
 }
